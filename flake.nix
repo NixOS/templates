ae3dde16680b4c1cf6a198c1c2bcfe086009b1d1
Eelco Dolstra <edolstra@gmail.com>
2020-06-04 20:20:02 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "A collection of flake templates";
+
+  outputs = { self }: {
+
+    templates = {
+
+      trivial = {
+        path = ./trivial;
+        description = "A very basic flake";
+      };
+
+    };
+
+    defaultTemplate = self.templates.trivial;
+
+  };
+}
