c31455c82aea96191b6b6a0bff4f4ff3263451b5
Eelco Dolstra <edolstra@gmail.com>
2020-06-23 15:34:52 +0200

    templates.simpleContainer: Add

--- flake.nix
+++ flake.nix
@@ -10,6 +10,11 @@
         description = "A very basic flake";
       };
 
+      simpleContainer = {
+        path = ./simple-container;
+        description = "A NixOS container running apache-httpd";
+      };
+
     };
 
     defaultTemplate = self.templates.trivial;
---
+++ simple-container/flake.nix
@@ -0,0 +1,28 @@
+{
+  outputs = { self, nixpkgs }: {
+
+    nixosConfigurations.container = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules =
+        [ ({ pkgs, ... }: {
+            boot.isContainer = true;
+
+            # Let 'nixos-version --json' know about the Git revision
+            # of this flake.
+            system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
+
+            # Network configuration.
+            networking.useDHCP = false;
+            networking.firewall.allowedTCPPorts = [ 80 ];
+
+            # Enable a web server.
+            services.httpd = {
+              enable = true;
+              adminAddr = "morty@example.org";
+            };
+          })
+        ];
+    };
+
+  };
+}
