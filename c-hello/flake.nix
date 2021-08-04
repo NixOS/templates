d6cdadfd6d91656fa46dec0be888f238d599ef15
Las Safin <me@las.rs>
2021-08-04 11:38:03 +0000

    η-reduction in forAllSystems

--- c-hello/flake.nix
+++ c-hello/flake.nix
@@ -14,7 +14,7 @@
       supportedSystems = [ "x86_64-linux" "x86_64-darwin" ];
 
       # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
-      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
 
       # Nixpkgs instantiated for supported system types.
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlay ]; });
