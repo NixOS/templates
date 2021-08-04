44245170e227aa71cfb489c13c1c950b2fac8242
Las Safin <me@las.rs>
2021-08-04 11:46:56 +0000

    Support aarch64-{linux,darwin} by default

--- c-hello/flake.nix
+++ c-hello/flake.nix
@@ -11,7 +11,7 @@
       version = builtins.substring 0 8 self.lastModifiedDate;
 
       # System types to support.
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" ];
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
 
       # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
       forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
