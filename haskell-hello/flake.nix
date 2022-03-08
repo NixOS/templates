53418d1b8f5cb55c62149b6370b9fbeec8125a82
shawn-bachlet <shawn.bachlet@simspace.com>
2022-03-08 11:05:28 -0500

    Add support for x86_64-darwin

--- haskell-hello/flake.nix
+++ haskell-hello/flake.nix
@@ -4,7 +4,7 @@
   inputs.nixpkgs.url = "nixpkgs";
   outputs = { self, nixpkgs }:
     let
-      supportedSystems = [ "x86_64-linux" ];
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" ];
       forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
       nixpkgsFor = forAllSystems (system: import nixpkgs {
         inherit system;
