1afb35ade1071921943c14b743ae8726ac8969c8
Yvan Sraka <yvan.sraka@iohk.io>
2022-08-08 10:24:20 +0200

    Add haskell.nix template
    https://github.com/input-output-hk/haskell.nix

--- flake.nix
+++ flake.nix
@@ -77,6 +77,15 @@
         description = "A simple Go package";
       };
 
+      haskell.nix = {
+        path = ./go-hello;
+        description = "An haskell.nix template using hix";
+        welcomeText = ''
+          You just created an haskell.nix template using hix. Read more about it here:
+          https://input-output-hk.github.io/haskell.nix/tutorials/getting-started-flakes.html
+        '';
+      };
+
     };
 
     defaultTemplate = self.templates.trivial;
