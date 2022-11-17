6ca9b8b02e3c0e14ad2fffd812febbde5797bf07
Matthieu Coudron <mcoudron@hotmail.com>
2022-11-17 12:29:56 +0100

    add nixpkgs as input of "trivial" template
    I had to give a nix training yesterday trying to explain nix, how flakes
    are purer than nix <2.4 than I asked the students to run `nix flake init`
    which doesnt generate any input.
    It forces to introduce the concept of registry and makes it less "pure"
    as now your inputs are hidden.
    I add the inputs here to make it more newcomer friendly, and I think it
    makes developinbg your own flake easier with inputs laid out fromn the
    start.
    see https://github.com/NixOS/nix/issues/7005

--- trivial/flake.nix
+++ trivial/flake.nix
@@ -1,6 +1,10 @@
 {
   description = "A very basic flake";
 
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
   outputs = { self, nixpkgs }: {
 
     packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
