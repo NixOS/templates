7fe47be35ea181cf9dc4a9de1227b3d41830b1e3
Tom Bereknyei <tomberek@gmail.com>
2024-08-17 03:20:52 -0400

    bump: ruby deps

--- ruby/flake.nix
+++ ruby/flake.nix
@@ -3,7 +3,7 @@
   # Useage: see README.md
   
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
 };
 
