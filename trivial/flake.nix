2e8aa8a265fd2374d12b2bb091797c46d4370910
Luc Perkins <lucperkins@gmail.com>
2025-08-14 17:45:03 +0100

    Make the trivial flake multi system

--- trivial/flake.nix
+++ trivial/flake.nix
@@ -1,15 +1,38 @@
 {
   description = "A very basic flake";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
+  outputs =
+    { self, nixpkgs }:
+    let
+      # Systems supported by this flake
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      # A helper function for specifying per-system outputs
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import nixpkgs { inherit system; };
+          }
+        );
+    in
+    {
+      # Package outputs
+      packages = forEachSupportedSystem (
+        { pkgs }:
+        {
+          # Build this by running `nix build`, `nix build .`, or `nix build .#default`
+          default = pkgs.hello;
+          # Build this by running `nix build .#hello`
+          hello = pkgs.hello;
+        }
+      );
+    };
 }
