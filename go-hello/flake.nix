b0911a7e95f232b78b6785d039b2272a0aa6e159
ElrohirGT <45268815+ElrohirGT@users.noreply.github.com>
2026-09-13 17:21:49 -0600

    fix: Updated the Go template to use Go 1.23 instead of 1.16! (#89)
    * fix: Updated go template
    
    * fix: Removed vendorHash as suggested by nix build error
    
    * fix: flake package
    
    ---------
    
    Co-authored-by: tomberek <tomberek@users.noreply.github.com>

--- go-hello/flake.nix
+++ go-hello/flake.nix
@@ -2,7 +2,7 @@
   description = "A simple Go package";
 
   # Nixpkgs / NixOS version to use.
-  inputs.nixpkgs.url = "https://channels.nixos.org/nixos-21.11/nixexprs.tar.zst";
+  inputs.nixpkgs.url = "https://channels.nixos.org/nixos-26.05/nixexprs.tar.zst";
 
   outputs = { self, nixpkgs }:
     let
@@ -48,7 +48,7 @@
             # remember to bump this hash when your dependencies change.
             # vendorHash = pkgs.lib.fakeHash;
 
-            vendorHash = "sha256-pQpattmS9VmO3ZIQUFn66az8GSmB4IvYhTTCFn6SUmo=";
+            vendorHash = null;
           };
         });
 
--- go-hello/go.mod
+++ go-hello/go.mod
@@ -1,3 +1,3 @@
 module example.com/go-hello
 
-go 1.16
+go 1.23
